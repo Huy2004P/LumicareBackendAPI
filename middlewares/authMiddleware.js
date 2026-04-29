a4fd97143babca49323e85b2762be8c778343009
Van Ba Phat Huy <phathuy2004h@gmail.com>
2026-04-29 10:30:46 +0700

    change final feature

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -3,19 +3,16 @@ const userRepository = require('../repositories/UserRepository');
 
 const verifyAccount = async (req, res, next) => {
     try {
-        // 1. Lấy token từ Header
+        // 1. Lấy token từ header Authorization
         const token = req.headers.authorization?.split(" ")[1];
         if (!token) {
             return res.status(401).json({ message: "Bạn chưa đăng nhập!" });
         }
-
         // 2. Giải mã token để lấy userId
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
         const userId = decoded.id;
-
-        // 3. 🎯 TRUY QUÉT DATABASE: Kiểm tra xem active còn bằng 1 hay không
+        // 3. Kiểm tra active của tài khoản
         const isActive = await userRepository.isAccountActive(userId);
-
         if (!isActive) {
             // Nếu active = 0, trả về 403 để Client tự động đá người dùng ra
             return res.status(403).json({
@@ -24,7 +21,6 @@ const verifyAccount = async (req, res, next) => {
                 message: "Tài khoản đã bị khóa. Vui lòng liên hệ Admin!"
             });
         }
-
         // Nếu mọi thứ ổn, kẹp userId vào request để xài ở Controller
         req.user = decoded;
         next();
